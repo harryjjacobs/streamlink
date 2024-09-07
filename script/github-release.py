c30bf6cbac0d195a9871537d1fdcf66578a1a5a5
harryjjacobs <harryjjacobs@googlemail.com>
2024-09-07 11:23:02 +0100

    stripped down release script

--- script/github-release.py
+++ script/github-release.py
@@ -25,18 +25,24 @@ ROOT = Path(__file__).parents[1].resolve()
 DEFAULT_REPO = "streamlink/streamlink"
 
 
-RE_CHANGELOG = re.compile(r"""
+RE_CHANGELOG = re.compile(
+    r"""
     ##\sstreamlink\s+
     (?P<version>\d+\.\d+\.\d+(?:-\S+)?)\s+
     \((?P<date>\d{4}-\d\d-\d\d)\)\n\n
     (?P<changelog>.+?)\n\n
     \[Full\schangelog]\(\S+\)\n+
     (?=\#\#\sstreamlink|$)
-""", re.VERBOSE | re.DOTALL | re.IGNORECASE)
+""",
+    re.VERBOSE | re.DOTALL | re.IGNORECASE,
+)
 
-RE_CO_AUTHOR = re.compile(r"""
+RE_CO_AUTHOR = re.compile(
+    r"""
     ^\s*Co-Authored-By:\s+(?P<name>.+)\s+<(?P<email>.+?@.+?)>\s*$
-""", re.VERBOSE | re.MULTILINE | re.IGNORECASE)
+""",
+    re.VERBOSE | re.MULTILINE | re.IGNORECASE,
+)
 
 
 def get_args():
@@ -162,7 +168,9 @@ class GitHubAPI:
         self._get_api_key()
 
     def _get_api_key(self):
-        github_token, releases_api_key = getenv("GITHUB_TOKEN"), getenv("RELEASES_API_KEY")
+        github_token, releases_api_key = getenv("GITHUB_TOKEN"), getenv(
+            "RELEASES_API_KEY"
+        )
 
         # use the GitHub actions token (no authentication check necessary/possible)
         if github_token:
@@ -190,7 +198,11 @@ class GitHubAPI:
         raise_failure: bool = True,
         **kwargs,
     ) -> requests.Response:
-        func = requests.post if method == "POST" else requests.patch if method == "PATCH" else requests.get
+        func = (
+            requests.post
+            if method == "POST"
+            else requests.patch if method == "PATCH" else requests.get
+        )
 
         response: requests.Response = func(
             f"https://{host}{endpoint}",
@@ -199,7 +211,9 @@ class GitHubAPI:
         )
         if raise_failure and response.status_code >= 400:
             log.debug(f"GitHub API request failed:\n{response.text}")
-            raise requests.HTTPError(f"GitHub API request {method} {endpoint} returned {response.status_code}")
+            raise requests.HTTPError(
+                f"GitHub API request {method} {endpoint} returned {response.status_code}"
+            )
 
         return response
 
@@ -225,7 +239,9 @@ class GitHubAPI:
 
     def create_release(self, payload: dict) -> Optional[int]:
         if not self.authenticated:
-            log.info(f"dry-run: Would have created GitHub release {self.repo}#{self.tag} with:\n{pformat(payload)}")
+            log.info(
+                f"dry-run: Would have created GitHub release {self.repo}#{self.tag} with:\n{pformat(payload)}"
+            )
             return
 
         log.info(f"Creating new GitHub release {self.repo}#{self.tag}")
@@ -240,7 +256,9 @@ class GitHubAPI:
 
     def update_release(self, release_id: int, payload: dict) -> None:
         if not self.authenticated:
-            log.info(f"dry-run: Would have updated GitHub release {self.repo}#{self.tag} with:\n{pformat(payload)}")
+            log.info(
+                f"dry-run: Would have updated GitHub release {self.repo}#{self.tag} with:\n{pformat(payload)}"
+            )
             return
 
         log.info(f"Updating existing GitHub release {self.repo}#{self.tag}")
@@ -264,7 +282,9 @@ class GitHubAPI:
 
     def upload_asset(self, release_id: int, filename: str, filehandle: IO):
         if not self.authenticated:
-            log.info(f"dry-run: Would have uploaded '{filename}' to GitHub release {self.repo}#{self.tag}")
+            log.info(
+                f"dry-run: Would have uploaded '{filename}' to GitHub release {self.repo}#{self.tag}"
+            )
             return
 
         log.info(f"Uploading '{filename}' to GitHub release {self.repo}#{self.tag}")
@@ -276,127 +296,14 @@ class GitHubAPI:
             params={"name": filename},
             data=filehandle,
         )
-        log.info(f"Successfully uploaded '{filename}' to GitHub release {self.repo}#{self.tag}")
-
-    def get_contributors(self, start: str, end: str) -> list[Author]:
-        log.debug(f"Getting contributors of {self.repo} in commit range {start}...{end}")
-
-        authors: dict[Email, Author] = {}
-        co_authors: list[Email] = []
-
-        total_commits = None
-        parsed_commits = 0
-        page = 0
-
-        while total_commits is None or parsed_commits < total_commits:
-            page += 1
-            res = self.call(
-                endpoint=f"/repos/{self.repo}/compare/{start}...{end}",
-                params={
-                    "page": page,
-                    "per_page": self.PER_PAGE,
-                },
-            )
-            if res.status_code != 200:
-                raise requests.HTTPError(f"Status code {res.status_code} for request {res.url}")
-
-            data: dict = res.json()
-
-            if total_commits is None:
-                total_commits = data.get("total_commits")
-                if total_commits is None:
-                    raise ValueError("Could not get total_commits value")
-                if total_commits > self.MAX_REQUESTS * self.PER_PAGE:
-                    raise ValueError("Too many commits in input range")
-
-            commits: list[dict] = data.get("commits", [])
-            parsed_commits += len(commits)
-
-            for commitdata in commits:
-                commit = commitdata.get("commit") or {}
-
-                # ignore merge commits
-                if len(commitdata.get("parents") or []) > 1:
-                    continue
-
-                # GitHub identifies its users by checking the commit-author's email address
-                commit_author_email = Email((commit.get("author") or {}).get("email"))
-                # The commit-author's name can differ from the GitHub user account name -> use the provided author login
-                author_name = (commitdata.get("author") or {}).get("login")
-                if not commit_author_email or not author_name:
-                    continue
-
-                if commit_author_email not in authors:
-                    authors[commit_author_email] = Author(commit_author_email, author_name)
-                authors[commit_author_email].commits += 1
-
-                # Co-Author data can be embedded in the commit message
-                # This data can only be used if the attached email address exists in other commits of the input range, as the
-                # data is arbitrary and doesn't include GitHub user login names
-                for item in re.finditer(RE_CO_AUTHOR, commit.get("message", "")):
-                    co_author_email = Email(item.group("email"))
-                    # Ignore Co-Author data if it's the actual commit-author
-                    if co_author_email == commit_author_email:
-                        continue
-                    co_authors.append(co_author_email)
-
-        # Look for any existing commit-author-email-addresses for each co-author
-        for email in co_authors:
-            if email in authors:
-                # and increase their commit count by one
-                authors[email].commits += 1
-
-        # sort by commits in descending order and by login name in ascending order
-        return sorted(
-            sorted(
-                authors.values(),
-                key=lambda author: author.name,
-                reverse=False,
-            ),
-            key=lambda author: author.commits,
-            reverse=True,
+        log.info(
+            f"Successfully uploaded '{filename}' to GitHub release {self.repo}#{self.tag}"
         )
 
 
 class Release:
-    def __init__(self, tag: str, template: Path, changelog: Path):
+    def __init__(self, tag: str, template: Path):
         self.tag = tag
-        self.template = template
-        self.changelog = changelog
-
-    @staticmethod
-    def _read_file(path: Path):
-        with open(path, "r", encoding="utf-8") as fh:
-            contents = fh.read()
-
-        if not contents:
-            raise IOError()
-
-        return contents
-
-    def _read_template(self):
-        log.debug(f"Opening release template file: {self.template}")
-        try:
-            return self._read_file(self.template)
-        except OSError as err:
-            raise OSError("Missing release template file") from err
-
-    def _read_changelog(self):
-        log.debug(f"Opening changelog file: {self.changelog}")
-        try:
-            return self._read_file(self.changelog)
-        except OSError as err:
-            raise OSError("Missing changelog file") from err
-
-    def _get_changelog(self) -> dict:
-        changelog = self._read_changelog()
-
-        log.debug("Parsing changelog file")
-        for match in re.finditer(RE_CHANGELOG, changelog):
-            if match.group("version") == self.tag:
-                return match.groupdict()
-
-        raise KeyError("Missing changelog for current release")
 
     @staticmethod
     @contextmanager
@@ -414,38 +321,6 @@ class Release:
             for handle in handles.values():
                 handle.close()
 
-    def get_body(
-        self,
-        api: GitHubAPI,
-        no_contributors: bool = False,
-        no_shortlog: bool = False,
-    ) -> str:
-        template = self._read_template()
-        jinjatemplate = jinja2.Template(template)
-
-        changelog = self._get_changelog()
-        context = dict(**changelog)
-
-        if not no_contributors or not no_shortlog:
-            # don't include the tagged release commit
-            prev_commit = f"{self.tag}~1"
-
-            # get the previous tag
-            start = Git.tag(prev_commit)
-            if not start:
-                raise ValueError(f"Could not resolve tag from reference {prev_commit}")
-
-            if not no_contributors:
-                context.update(
-                    contributors=api.get_contributors(start, prev_commit),
-                )
-            if not no_shortlog:
-                context.update(
-                    gitshortlog=Git.shortlog(start, prev_commit),
-                )
-
-        return jinjatemplate.render(context)
-
 
 def main(args: argparse.Namespace):
     # if no tag was provided, get the current tag from `git describe --tags`
@@ -456,20 +331,17 @@ def main(args: argparse.Namespace):
     log.info(f"Repo: {args.repo}")
     log.info(f"Tag: {tag}")
 
-    release = Release(tag, args.template, args.changelog)
+    release = Release(tag)
 
     # get file handles of release assets first, to prevent unnecessary API requests if input files can't be found
     with release.get_file_handles(args.assets) as filehandles:
         # initialize GitHub API
         api = GitHubAPI(args.repo, tag)
 
-        # prepare the release body with the changelog, contributors list and git shortlog
-        body = release.get_body(api, args.no_contributors, args.no_shortlog)
-
         # create a new release or update an existing one with the same tag
         release_id = api.create_or_update_release(
             name=f"Streamlink {tag}",
-            body=body,
+            body="",
         )
 
         # upload assets
