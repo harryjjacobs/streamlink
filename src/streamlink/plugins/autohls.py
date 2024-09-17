5ca75d11a640f1eaa1730474765f6defae3d927d
harryjjacobs <harryjjacobs@googlemail.com>
2024-09-17 22:53:42 +0100

    some plugin fixes

--- src/streamlink/plugins/autohls.py
+++ src/streamlink/plugins/autohls.py
@@ -7,6 +7,7 @@ $region global
 
 import logging
 import re
+import subprocess
 import time
 
 from streamlink.plugin import Plugin, pluginmatcher
@@ -20,12 +21,32 @@ log = logging.getLogger(__name__)
 class NetworkRequestsListener:
     def __init__(self, page):
         self.requests = []
-        page.on("request", self.on_request)
+        self.page = page
 
-    def on_request(self, request):
-        if "m3u8" in request.url:
+    def __enter__(self):
+        self.page.on("requestfinished", self.on_requestfinished)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.page.remove_listener("requestfinished", self.on_requestfinished)
+
+    def on_requestfinished(self, request):
+        # print(request.url)
+        if self.page.is_closed():
+            return
+        if self.is_m3u8_request(request):
             self.requests.append(request)
 
+    def is_m3u8_request(self, request):
+        if "m3u8" in request.url:
+            return True
+        try:
+            return "#EXT-X-MEDIA-SEQUENCE" in request.response().text()
+        except UnicodeDecodeError:
+            return False
+        except Exception as e:
+            log.error(f"Failed to check if request is m3u8: {e}")
+            return False
 
 @pluginargument(
     "show-browser",
@@ -57,10 +78,12 @@ class AutoHLSPlugin(Plugin):
         try:
             import playwright
         except ImportError:
-            self.logger.error(
-                "Playwright is not installed. Please install it by running 'pip install playwright'."
+            self.logger.warn(
+                "Playwright is not installed. Installing it by running 'pip install playwright'."
             )
             return []
+
+        subprocess.run(["playwright", "install"])
 
         from playwright.sync_api import sync_playwright
 
@@ -70,18 +93,34 @@ class AutoHLSPlugin(Plugin):
             try:
                 context = browser.new_context()
                 page = context.new_page()
-                page.goto(self.url, wait_until="domcontentloaded")
-                page.wait_for_load_state("load")
-                # listen to network requests for m3u8 files
-                listener = NetworkRequestsListener(page)
-                # wait for a while to capture all requests
-                self.logger.info("Auto-detecting HLS streams...")
-                time.sleep(5)
-                return listener.requests
+                with NetworkRequestsListener(page) as listener:
+                    page.goto(self.url, wait_until="domcontentloaded")
+                    page.wait_for_load_state("load")
+                    # listen to network requests for m3u8 files
+                    # activate video player if present
+                    self._activate_video_player(browser, context, page)
+                    # wait for a while to capture all requests
+                    self.logger.info("Auto-detecting HLS streams...")
+                    time.sleep(5)
+                    return listener.requests
             except Exception as e:
                 log.error(f"Failed to load page: {e}")
             finally:
                 browser.close()
 
+    def _activate_video_player(self, browser, context, page):
+        # if the page has a video player element, activate it
+        # by clicking on it
+        video_player = page.query_selector("video")
+        if video_player:
+            try:
+                video_player.click()
+            except Exception as e:
+                log.error(f"Failed to click on video player: {e}")
+            return True
+        return False
+
+    # TODO: some of these sites hide the m3u8 URL with a css file extension
+    # We could look at the content of the css file to detect if it's a m3u8 file
 
 __plugin__ = AutoHLSPlugin
